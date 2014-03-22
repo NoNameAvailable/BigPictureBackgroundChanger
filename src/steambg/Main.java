c30dd9fe82106df0a121e6b8e4fa052c3f28c2b5
NoNameAvailable <hockeypattemore@gmail.com>
2014-03-21 21:38:22 -0400

    Update to allow program to automatically check if Steam is running
    It is a bit hacky in the way it works at the moment, (I just gave it a
    20s timer  after steam starts before it starts working before changing
    the background), but it does finally work at changing the Big Picture
    Mode Background.

--- src/steambg/Main.java
+++ src/steambg/Main.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package steambg;
-
-import java.awt.image.BufferedImage;
-import java.io.*;
-import javax.imageio.ImageIO;
-
 /**
  *
  * @author James Pattemore
@@ -15,12 +5,27 @@ import javax.imageio.ImageIO;
  * This is an application that changes the default background of Big Picture mode to
  * one of the users choosing.
  */
-public class Main {
 
-    /**
-     * @param args the command line arguments
+package steambg;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+
+
+public class Main {
+    /* This is used for the checker function. since I cant get that to work, I 
+     * don't have a need for this at this time.
      */
-    public static void main(String[] args) {
+    static boolean isSteamRunning=false;
+    
+
+    /* I should Probably put the interface elements somewhere else, but for now
+     * they are right here.
+     */
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
         //Creates the custom background folder. would like to have this set in a gui in the future.
         File bgFolder = new File("C:\\SteamCustomBackGround");
         // Checks if the background folder exists, if not the folder is created.
@@ -29,6 +34,48 @@ public class Main {
         else {
             bgFolder.mkdir();
         }
+        while (isSteamRunning==false){
+            checker(isSteamRunning);
+        }
+        if (isSteamRunning==true){
+            loader();
+        }
+        
+        
+    }
+
+/* I tried to make this work and failed. I'll come back to it in the furture, bot for now, I'm focusing
+* on other aspects
+*/
+    public static void checker (Boolean sRunning) throws IOException, InterruptedException {
+
+            String line;
+            try {
+                Process proc = Runtime.getRuntime().exec("wmic.exe");
+                BufferedReader input = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                OutputStreamWriter oStream = new OutputStreamWriter(proc.getOutputStream());
+                oStream .write("process where name='steam.exe'");
+                oStream .flush();
+                oStream .close();
+                if (sRunning==false){
+                    Thread.sleep(1000);
+                    while ((line = input.readLine()) !=null) {
+                        if (line.contains("Steam.exe")){
+                            Thread.sleep(20000);
+                            System.out.println(line);
+                            sRunning=true;
+                        }
+                    }
+                }
+                input.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            isSteamRunning=sRunning;
+            System.out.println(isSteamRunning);
+    }
+
+    public static void loader () {
         // Load the Image the person selected into Java, then set as big picture mode background.
         BufferedImage bgImg = null;
             try {
@@ -41,5 +88,4 @@ public class Main {
             catch (IOException e) {
             }
     }
-
 }
